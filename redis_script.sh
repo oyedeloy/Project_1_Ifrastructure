45b0753d3216208e416fc33d15ee1ad1821ae32a
dele <oyedeloy@gmail.com>
2020-11-21 15:38:03 -0600

    redis and monitor ami apdated

--- redis_script.sh
+++ redis_script.sh
@@ -7,4 +7,3 @@ sudo systemctl enable docker
 sudo curl -L https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose 
 sudo chmod +x /usr/local/bin/docker-compose 
 sudo cp /usr/local/bin/docker-compose /usr/bin/
-sudo docker-compose up -d
