ef8a5bd65792ae2515921c49fc3fb23dcca6e8ea
dele <oyedeloy@gmail.com>
2020-11-24 21:19:15 -0600

    infra

--- instances.tf
+++ instances.tf
@@ -141,7 +141,8 @@ output "redis4_ip" {
 }
 
 resource "aws_instance" "Infra_toolbox" {
-  ami                    = "ami-095c1dc13c630f1fe"
+  ami                    = "ami-0570e008991181708
+"
   instance_type          = "t2.micro"
   subnet_id              = aws_subnet.public_subnet.id
   key_name               = "devops-key2"
